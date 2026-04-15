6a26aefaa11541f24b00d8ba1fc4f0525b56e838
shreyash2256 <shreyashbhatt43@gmail.com>
2026-04-15 21:42:02 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter_practicals\\flutter ala irrelevant\\flutter_ala3" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\src\\flutter"
+  "PROJECT_DIR=D:\\flutter_practicals\\flutter ala irrelevant\\flutter_ala3"
+  "FLUTTER_ROOT=D:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter_practicals\\flutter ala irrelevant\\flutter_ala3\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter_practicals\\flutter ala irrelevant\\flutter_ala3"
+  "FLUTTER_TARGET=D:\\flutter_practicals\\flutter ala irrelevant\\flutter_ala3\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZGI1MGUyMDE2OA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NDI1Y2ZiNTRkMA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS40"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter_practicals\\flutter ala irrelevant\\flutter_ala3\\.dart_tool\\package_config.json"
+)
